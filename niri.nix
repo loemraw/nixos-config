abc6274c07c509d87572682af2f82f030cba8559
Leo Martins <loemra.dev@gmail.com>
2025-11-12 23:36:00 -0800

    niri: fix formatting

--- niri.nix
+++ niri.nix
@@ -7,15 +7,15 @@
       input = {
         keyboard = {
           xkb = {
-	    layout = "us";
-	    options = "ctrl:nocaps";
-	  };
-	};
+            layout = "us";
+            options = "ctrl:nocaps";
+          };
+        };
       };
 
       layout = {
         gaps = 8;
-	center-focused-column = "never";
+        center-focused-column = "never";
       };
 
       spawn-at-startup = [
@@ -24,34 +24,33 @@
       ];
 
       binds = {
-        "Mod+Space".action.spawn = ["fuzzel"];
-	"Mod+Return".action.spawn = ["alacritty"];
-	"Mod+B".action.spawn = ["firefox"];
-	"Mod+Q".action.close-window = [];
-	"Mod+F".action.fullscreen-window = [];
+        "Mod+Space".action.spawn = [ "fuzzel" ];
+        "Mod+Return".action.spawn = [ "alacritty" ];
+        "Mod+B".action.spawn = [ "firefox" ];
+        "Mod+Q".action.close-window = [ ];
+        "Mod+F".action.fullscreen-window = [ ];
 
         # Navigation
-        "Mod+Left".action.focus-column-left = [];
-        "Mod+Right".action.focus-column-right = [];
-        "Mod+Up".action.focus-window-up = [];
-        "Mod+Down".action.focus-window-down = [];
-        
+        "Mod+Left".action.focus-column-left = [ ];
+        "Mod+Right".action.focus-column-right = [ ];
+        "Mod+Up".action.focus-window-up = [ ];
+        "Mod+Down".action.focus-window-down = [ ];
+
         # Move
-        "Mod+Shift+Left".action.move-column-left = [];
-        "Mod+Shift+Right".action.move-column-right = [];
-        
+        "Mod+Shift+Left".action.move-column-left = [ ];
+        "Mod+Shift+Right".action.move-column-right = [ ];
+
         # Workspaces
         "Mod+1".action.focus-workspace = 1;
         "Mod+2".action.focus-workspace = 2;
         "Mod+3".action.focus-workspace = 3;
 
-	# Monitors
-	"Mod+Ctrl+Left".action.focus-monitor-left = [];
-	"Mod+Ctrl+Right".action.focus-monitor-right= [];
+        # Monitors
+        "Mod+Ctrl+Left".action.focus-monitor-left = [ ];
+        "Mod+Ctrl+Right".action.focus-monitor-right = [ ];
 
-        
         # Exit niri
-        "Mod+Shift+E".action.quit = [];
+        "Mod+Shift+E".action.quit = [ ];
       };
 
       prefer-no-csd = true;
