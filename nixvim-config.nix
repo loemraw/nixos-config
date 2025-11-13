24489b2066ed22299493f77a7092bcf6d26eeb97
Leo Martins <loemra.dev@gmail.com>
2025-11-12 22:27:50 -0800

    neovim: set live_grep to fs and remove unused keybinds

--- nixvim-config.nix
+++ nixvim-config.nix
@@ -236,9 +236,7 @@
         enable = true;
         keymaps = {
           "<leader>ff" = "find_files";
-          "<leader>fg" = "live_grep";
-          "<leader>fb" = "buffers";
-          "<leader>fh" = "help_tags";
+          "<leader>fs" = "live_grep";
         };
       };
 
